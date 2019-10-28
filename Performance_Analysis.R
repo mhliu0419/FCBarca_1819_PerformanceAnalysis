4de65cdef22137c1627e13537f0f9edccc02d13a
Minghao Liu <35540144+mhliu0419@users.noreply.github.com>
2019-10-28 17:31:38 -0400

    Update Performance_Analysis.R

--- Performance_Analysis.R
+++ Performance_Analysis.R
@@ -18,4 +18,7 @@ Total_Data$Date <- as.Date(Total_Data$Date, format = "%d/%m/%Y")
 Barca_Total_Data <- arrange(Total_Data, Date)
 
 
-ggplot(Barca_Away_Data, aes(x=Date,y=FTAG)) + geom_point() + geom_text(aes(label = HomeTeam, check_overlap = TRUE))
+ggplot(Barca_Away_Data, aes(x=Date,y=FTAG)) + geom_point() + geom_text(aes(label = HomeTeam), check_overlap = TRUE) + scale_x_date(date_breaks = '2 week')
+
+ggplot(Barca_Home_Data, aes(x=Date,y=FTHG)) + geom_point() + geom_text(aes(label = AwayTeam), check_overlap = TRUE) + scale_x_date(date_breaks = '2 week')
+
